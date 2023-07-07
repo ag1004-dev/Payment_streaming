52ea0f35225e14f5c37e5870478f5462af105853
SmartDev116 <williambennett116ok@proton.me>
2023-07-07 04:27:29 +0300

    test: deposit coins into stream (#53)
    * Feat: Updating package-lock.json
    
    * Feat: Tests for function 'createOpenStream();'
    
    * Feat: Tests for function 'deposit();'
    
    ---------
    
    Co-authored-by: Vadim <1125014+zolotokrylin@users.noreply.github.com>

--- test/StreamManager.test.js
+++ test/StreamManager.test.js
@@ -8,7 +8,8 @@ describe("StreamManager", function () {
     this.payer = payer
     this.payee1 = payee1
     this.payee2 = payee2
-    this.zero = ethers.constants.AddressZero;
+    this.zero = ethers.constants.AddressZero
+    this.amount = 1000
 
     // Deploy StreamManager
     const StreamManager = await ethers.getContractFactory("StreamManager")
@@ -41,7 +42,6 @@ describe("StreamManager", function () {
   // Tests for `createOpenStream();`
   // Creating stream
   it('Creating an open stream instance succeed;', async () => {
-
     await expect(this.streamManager.createOpenStream(
       this.payee1.address,
       this.mockUSDT.address,
@@ -112,5 +112,57 @@ describe("StreamManager", function () {
         0
       )
     ).to.be.revertedWith('InvalidValue');
+  })
+
+  // Tests for `deposit();`
+  // Deposit USDT(mock)
+  it('Deposit succeed;', async () => {
+
+    await this.mockUSDT.mint(this.payer.address, this.amount)
+
+    await this.mockUSDT.connect(this.payer).approve(this.streamManager.address, this.amount)
+
+    await expect(
+      this.streamManager.connect(this.payer).deposit(
+      this.mockUSDT.address,
+      this.amount
+    ))
+    .to.emit(this.streamManager, "TokensDeposited")
+    .withArgs(this.mockUSDT.address, this.amount)
+
+    expect(await this.mockUSDT.balanceOf(this.streamManager.address)).to.equal(this.amount)
+  })
+
+  // Expecing rever with `InvalidAddress`
+  it('Deposit: `_token` not set how address(0);', async () => {
+    // Setting `_token` = address(0)
+    await expect(
+      this.streamManager.connect(this.payer).deposit(
+      this.zero,
+      this.amount
+    ))
+    .to.be.revertedWith('InvalidAddress');
+  })
+
+  // Expecing rever with `InvalidValue`
+  it('Deposit: `_amount` not set how 0;', async () => {
+    // Setting `_amount` = 0
+    await expect(
+      this.streamManager.connect(this.payer).deposit(
+      this.mockUSDT.address,
+      0
+    ))
+    .to.be.revertedWith('InvalidValue');
+  })
+
+  // Expecing rever with `NotPayer`
+  it('Deposit: only payer can call this function;', async () => {
+    // Calling from other address
+    await expect(
+      this.streamManager.connect(this.payee1).deposit(
+      this.mockUSDT.address,
+      this.amount
+    ))
+    .to.be.revertedWith('NotPayer');
   })
 });
