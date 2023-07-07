46d2b88e2623012a4b07a9d586672ff2856b046b
SmartDev116 <williambennett116ok@proton.me>
2023-07-07 04:18:49 +0300

    test: create stream (#52)
    * Feat: Updating package-lock.json
    
    * Feat: Tests for function 'createOpenStream();'
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Vadim <1125014+zolotokrylin@users.noreply.github.com>

--- test/StreamManager.test.js
+++ test/StreamManager.test.js
@@ -8,6 +8,7 @@ describe("StreamManager", function () {
     this.payer = payer
     this.payee1 = payee1
     this.payee2 = payee2
+    this.zero = ethers.constants.AddressZero;
 
     // Deploy StreamManager
     const StreamManager = await ethers.getContractFactory("StreamManager")
@@ -22,7 +23,8 @@ describe("StreamManager", function () {
     )
   })
 
-  it('Minting to payer succeed', async () => {
+  // Minting USDT(mock)
+  it('Minting to payer succeed;', async () => {
     const amount = ethers.BigNumber.from("10000000000")
 
     await expect(this.mockUSDT.mint(this.payer.address, amount))
@@ -36,4 +38,79 @@ describe("StreamManager", function () {
     expect(await this.mockUSDT.balanceOf(this.payer.address)).to.equal(ethers.BigNumber.from("20000000000"))
   })
 
+  // Tests for `createOpenStream();`
+  // Creating stream
+  it('Creating an open stream instance succeed;', async () => {
+
+    await expect(this.streamManager.createOpenStream(
+      this.payee1.address,
+      this.mockUSDT.address,
+      1500,
+      30,
+      30
+    ))
+    .to.emit(this.streamManager, "StreamCreated")
+    .withArgs(this.admin.address, this.payee1.address)
+  })
+
+  // Expercing rever with `InvalidAddress`
+  it('Creating open stream instance: `_payee` and `_token` are not set as address(0);', async () => {
+    // Setting `_payee` = address(0)
+    await expect(
+      this.streamManager.createOpenStream(
+        this.zero,
+        this.mockUSDT.address,
+        1500,
+        30,
+        30
+      )
+    ).to.be.revertedWith('InvalidAddress');
+
+    // Setting `_token` = address(0)
+    await expect(
+      this.streamManager.createOpenStream(
+        this.payee1.address,
+        this.zero,
+        1500,
+        30,
+        30
+      )
+    ).to.be.revertedWith('InvalidAddress');
+  })
+
+  // Expercing rever with `InvalidValue`
+  it('Creating an open stream instance: `_rate`, `_terminationPeriod`, `_cliffPeriod` not set how 0;', async () => {
+    // Setting `_rate` = 0
+    await expect(
+      this.streamManager.createOpenStream(
+        this.payee1.address,
+        this.mockUSDT.address,
+        0,
+        30,
+        30
+      )
+    ).to.be.revertedWith('InvalidValue');
+
+    // Setting `_terminationPeriod` = 0
+    await expect(
+      this.streamManager.createOpenStream(
+        this.payee1.address,
+        this.mockUSDT.address,
+        1500,
+        0,
+        30
+      )
+    ).to.be.revertedWith('InvalidValue');
+
+    // Setting `_cliffPeriod` = 0
+    await expect(
+      this.streamManager.createOpenStream(
+        this.payee1.address,
+        this.mockUSDT.address,
+        1500,
+        30,
+        0
+      )
+    ).to.be.revertedWith('InvalidValue');
+  })
 });
