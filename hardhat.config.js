5e89c8eb3ded45dea0a4b68d164e91a4926bfe68
SmartDev116 <williambennett116ok@proton.me>
2023-07-11 10:27:27 +0300

    feat: solidity coverage package (#58)
    * Add solidity-coverage package
    
    * Config hardhat for test coverage
    
    * Add coverage directory to .gitignore

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@
 require("@nomiclabs/hardhat-ethers");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+require("solidity-coverage");
 
 module.exports = {
   solidity: "0.8.18",
